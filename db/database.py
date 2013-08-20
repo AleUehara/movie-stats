d2c74503d1f0af5d38210a71635ecc112027d51e
Ale Uehara <you@example.com>
2013-08-19 21:37:50 -0300

    created connections and querys on mongodb

--- db/database.py
+++ db/database.py
@@ -3,14 +3,25 @@ from pymongo import Connection, MongoClient
 class MongoDBConnection():
     def __init__(self):
         client = MongoClient('localhost', 27021)
+        db = client.test_database
+        self.mongo_collection = db.movie_collection
+
+    def insert_collection(self, jsonfile):
+        insert_id = self.mongo_collection.insert(jsonfile)
+
+    def select_one(self):
+        print self.mongo_collection.find_one()
+
+    def count(self):
+        print self.mongo_collection.count()
+
+    def all_rates(self):
+    	pass
 
 
-def main():
-    print "ok"
-    MongoDBConnection()
 
 #----------------------------------------
 #Init
 #----------------------------------------
 if __name__ == "__main__":
-    main()
+    MongoDBConnection()
